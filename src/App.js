178101cdac5adbe13393a96c27b8c2e9115a4628
Theu <info@matteocontini.com>
2020-03-30 19:14:50 +0200

    first scratch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import styles from './app.module.css';
+
+const App = () => {
+  return (
+    <div className={styles.mainWrapper}>
+      main
+    </div>
+  );
+}
+
+export default App;
