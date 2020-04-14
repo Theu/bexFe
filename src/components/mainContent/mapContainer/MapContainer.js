3ea78179cb9288e39f50cc284b99d1d6e7359ee3
Theu <info@matteocontini.com>
2020-04-14 18:41:20 +0200

    start to introduce sticky

--- src/components/mainContent/mapContainer/MapContainer.js
+++ src/components/mainContent/mapContainer/MapContainer.js
@@ -3,12 +3,16 @@ import PropTypes from 'prop-types';
 
 import Map from './map/Map'
 import Pointers from './pointers/Pointers';
+import Sticky from './Sticky';
 import styles from './mapContainer.module.scss';
 
 const MapContainer = props => {
     return (
         <div className={styles.wrapper}>
             <Map />
+            {/* <Sticky top={113}>
+                <Map />
+            </Sticky> */}
             <Pointers />
         </div>
     );
--- src/components/mainContent/mapContainer/map/Map.js
+++ src/components/mainContent/mapContainer/map/Map.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import styles from './map.module.scss'
 
 const Map = props => {
+    console.log('MAPPA');
     return (
         <div className={styles.wrapper}>
             map
