7dd2df972d336df4b9d0d0919b2b8caf26e9c158
Theu <info@matteocontini.com>
2020-04-19 16:38:25 +0200

    set style for map render, props for map

--- src/components/mainContent/mapContainer/map/Map.js
+++ src/components/mainContent/mapContainer/map/Map.js
@@ -1,17 +1,25 @@
-import React, { useEffect, useRef, useState } from 'react';
-import PropTypes from 'prop-types';
-import MapRender from './MapRender';
-import styles from './map.module.scss';
+import React, { useEffect, useRef, useState } from "react";
+import PropTypes from "prop-types";
+import MapRender from "./MapRender";
 
 const mapMock = {
-	center: [ 52.518409, 13.401063 ],
-	zoom: 15,
-	zoomControl: true
+  center: [52.519409, 13.38],
+  zoom: 13,
 };
 const Map = (props) => {
-	return <MapRender lat={mapMock.center[0]} long={mapMock.center[1]} zoom={mapMock.zoom} />;
+  return (
+    <MapRender
+      lat={mapMock.center[0]}
+      long={mapMock.center[1]}
+      zoom={mapMock.zoom}
+    />
+  );
 };
 
-Map.propTypes = {};
+Map.propTypes = {
+  lat: PropTypes.number.isRequired,
+  long: PropTypes.number.isRequired,
+  zoom: PropTypes.number.isRequired,
+};
 
 export default Map;
