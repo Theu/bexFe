a81514b597f2211da2208bd361d1b4ea01e142c9
Theu <info@matteocontini.com>
2020-04-21 19:09:01 +0200

    mock service introction of jsx in pointer

--- src/client/components/tourExplanation/geoLocator/mapContainer/pointers/Pointers.js
+++ src/client/components/tourExplanation/geoLocator/mapContainer/pointers/Pointers.js
@@ -1,36 +1,34 @@
-import React from "react";
-import styles from "./pointers.module.scss";
-import { tourMock } from "../../../../../../server/tourMock";
-import { alexanderPlatz } from "../../../../../../server/imgMock";
+import React from 'react';
+import styles from './pointers.module.scss';
+import { tourMock } from '../../../../../../server/tourMock';
 
-const mock = tourMock.pointOfInterest;
+const pof = tourMock.pointOfInterest;
 
 const Pointers = () => {
-  const pointOfInterest = mock.map((interest) => {
-    console.log("interest.img", interest.img);
-    if (interest.hasOwnProperty("img")) {
-      return (
-        <div className={styles.infoBox}>
-          <img
-            src={interest.img}
-            alt={interest.title}
-            className={styles.imageWrapper}
-          />
-          <div className={styles.title}>{interest.title}</div>
-          <div className={styles.text}>{interest.text}</div>
-        </div>
-      );
-    } else {
-      return (
-        <div className={styles.infoBox}>
-          <div className={styles.title}> {interest.title}</div>
-          <div className={styles.text}>{interest.text}</div>
-        </div>
-      );
-    }
-  });
+    const pointOfInterest = pof.map((interest) => {
+        if (interest.hasOwnProperty('img')) {
+            return (
+                <div className={styles.infoBox}>
+                    <img
+                        src={interest.img}
+                        alt={interest.title}
+                        className={styles.imageWrapper}
+                    />
+                    <div className={styles.title}>{interest.title}</div>
+                    <div className={styles.text}>{interest.text}</div>
+                </div>
+            );
+        } else {
+            return (
+                <div className={styles.infoBox}>
+                    <div className={styles.title}> {interest.title}</div>
+                    <div className={styles.text}>{interest.text}</div>
+                </div>
+            );
+        }
+    });
 
-  return <div className={styles.pointersWrapper}>{pointOfInterest}</div>;
+    return <div className={styles.pointersWrapper}>{pointOfInterest}</div>;
 };
 
 export default Pointers;
