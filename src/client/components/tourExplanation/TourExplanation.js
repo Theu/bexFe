e4f91871a5e37331bf9876477bc292fc1345a97e
mcontini <matteo.contini@zalando.de>
2020-05-21 12:56:02 +0200

    moved message outside

--- src/client/components/tourExplanation/TourExplanation.js
+++ src/client/components/tourExplanation/TourExplanation.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { tourMock } from './../../../server/tourMock';
 import PlanContainer from './geoLocator/mapContainer/plan/PlanContainer';
+import Message from './message/Message';
 import styles from './tourExplanation.module.scss';
 
 export const TourExplanation = () => {
     const { mapMock } = tourMock;
+
+    const [isPanelOpen, setIsPanelOpen] = useState(false);
+    const [hasCoord, setCoord] = useState({});
+    const onClickToggle = () => {
+        console.log('CLICK');
+        console.log('isPanelOpen :>> ', isPanelOpen);
+        setIsPanelOpen(!isPanelOpen);
+    };
     return (
         <div className={styles.wrapper}>
             <PlanContainer map={mapMock} />
+            <Message
+                onClickToggle={onClickToggle}
+                show={isPanelOpen}
+                coord={hasCoord}
+            />
         </div>
     );
 };
