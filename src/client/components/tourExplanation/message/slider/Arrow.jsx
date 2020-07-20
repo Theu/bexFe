02d94f9ed5f5e5fbd69dd263161680bcf6c8c0c3
mcontini <matteo.contini@zalando.de>
2020-07-20 18:14:33 +0200

    removed console.log

--- src/client/components/tourExplanation/message/slider/Arrow.jsx
+++ src/client/components/tourExplanation/message/slider/Arrow.jsx
@@ -2,14 +2,10 @@ import React from 'react';
 
 import { ArrowContainer } from './Arrow.styles';
 
-const Arrow = ({ direction, handleClick }) => {
-
-    console.log('direction :>> ', direction);
-    return (
-        <ArrowContainer direction={direction} onClick={handleClick}>
-            {direction === 'right' ? '>' : '<'}
-        </ArrowContainer>
-    );
-};
+const Arrow = ({ direction, handleClick }) => (
+    <ArrowContainer direction={direction} onClick={handleClick}>
+        {direction === 'right' ? '>' : '<'}
+    </ArrowContainer>
+);
 
 export default Arrow;
