5f6db50bb2462a9dcad1ffcf23de94439a5ca706
mcontini <matteo.contini@zalando.de>
2020-07-25 16:19:44 +0200

    clean

--- src/client/components/tourHomePage/TourHomePage.jsx
+++ src/client/components/tourHomePage/TourHomePage.jsx
@@ -5,18 +5,18 @@ import { tourMock } from '../../../server/tourMock';
 
 export const TourHomePage = () => {
     const cards = Object.values(tourMock);
-        const columns = isMobile(window.screen.availWidth) ? 1 : 5;
+    const columns = isMobile(window.screen.availWidth) ? 1 : 5;
 
-        return (
-            <div>
-                <div style={{ columnCount: columns }}>
-                    {cards &&
-                        cards.map((card, index) => (
-                            <Card key={index} card={card} />
-                        ))}
-                </div>
+    return (
+        <div>
+            <div style={{ columnCount: columns }}>
+                {cards &&
+                    cards.map((card, index) => (
+                        <Card key={index} card={card} />
+                    ))}
             </div>
-        );
+        </div>
+    );
 };
 
 export default TourHomePage;
