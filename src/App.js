033fcf0c7767b53be0cece65e3df519a8d3098c8
mcontini <matteo.contini@zalando.de>
2020-08-29 10:29:44 +0200

    app index pattern

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// redux. check if after context introduction this is still needed
+import store from './client/redux/store/configureStore';
+import { Provider } from 'react-redux';
+
+// context. let's see who win
+import { AuthContext } from './client/context/auth';
+
+import { tourMock } from '../src/server/tourMock';
+
+import PrivateRoute from './client/components/authentication/PrivateRoute';
+import Admin from './client/components/authentication/mockComponent/AdminMock';
+import LoginMock from './client/components/authentication/mockComponent/LoginMock';
+import LogIn from './client/components/authentication/authForm/LogIn';
+import SignUp from './client/components/authentication/authForm/SignUp';
+
+import Header from './client/components/header/Header';
+import TourHomePage from './client/components/tourHomePage/TourHomePage';
+import TourExplanation from './client/components/tourExplanation/TourExplanation';
+import './index.scss';
+
+const tourList = Object.keys(tourMock);
+
+export const App = (props) => {
+    return (
+        <React.StrictMode>
+            <Provider store={store}>
+                <AuthContext.Provider value={false}>
+                    <Router>
+                        <>
+                            <Header />
+                            <Switch>
+                                <Route
+                                    exact
+                                    path="/"
+                                    component={TourHomePage}
+                                />
+                                {tourList.map((path) => (
+                                    <Route
+                                        key={path}
+                                        path={`/${path}`}
+                                        component={TourExplanation}
+                                    />
+                                ))}
+                            </Switch>
+                            <Route path="/login" component={LogIn} />
+                            <Route path="/signup" component={SignUp} />
+                            <Route path="/loginmock" component={LoginMock} />
+                            <PrivateRoute path="/admin" component={Admin} />
+                        </>
+                    </Router>
+                </AuthContext.Provider>
+            </Provider>
+        </React.StrictMode>
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,60 +1,12 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-// redux. check if after context introduction this is still needed
-import store from './client/redux/store/configureStore';
-import { Provider } from 'react-redux';
-
-// context. let's see who win
-import { AuthContext } from './client/context/auth';
-
-import { tourMock } from '../src/server/tourMock';
-
-import PrivateRoute from './client/components/authentication/PrivateRoute';
-import Admin from './client/components/authentication/mockComponent/AdminMock';
-import LoginMock from './client/components/authentication/mockComponent/LoginMock';
-import LogIn from './client/components/authentication/authForm/LogIn';
-import SignUp from './client/components/authentication/authForm/SignUp';
-
-import Header from './client/components/header/Header';
-import TourHomePage from './client/components/tourHomePage/TourHomePage';
-import TourExplanation from './client/components/tourExplanation/TourExplanation';
+import ReactDOM from 'react-dom';
 import './index.scss';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const tourList = Object.keys(tourMock);
-
-render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <AuthContext.Provider value={false}>
-                <Router>
-                    <>
-                        <Header />
-                        <Switch>
-                            <Route exact path="/" component={TourHomePage} />
-                            {tourList.map((path) => (
-                                <Route
-                                    key={path}
-                                    path={`/${path}`}
-                                    component={TourExplanation}
-                                />
-                            ))}
-                        </Switch>
-                        <Route path="/login" component={LogIn} />
-                        <Route path="/signup" component={SignUp} />
-                        <Route path="/loginmock" component={LoginMock} />
-                        <PrivateRoute path="/admin" component={Admin} />
-                    </>
-                </Router>
-            </AuthContext.Provider>
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// import * as serviceWorker from './serviceWorker';
-// serviceWorker.unregister();
+serviceWorker.unregister();
