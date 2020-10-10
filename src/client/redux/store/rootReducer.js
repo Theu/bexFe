05278c4848e0234aa78dfca4ac663ec580c21ce8
mcontini <matteo.contini@zalando.de>
2020-10-10 13:04:02 +0200

    cleaned unsed redux and methods

--- src/client/redux/store/rootReducer.js
+++ src/client/redux/store/rootReducer.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import coordReducer from '../modules/coords/reducer';
 import panelReducer from '../modules/panel/reducer';
-import sizesReducer from '../modules/clientSizes/reducer';
 
 const rootReducer = combineReducers({
     coords: coordReducer,
     panel: panelReducer,
-    sizes: sizesReducer,
 });
 
 export default rootReducer;
