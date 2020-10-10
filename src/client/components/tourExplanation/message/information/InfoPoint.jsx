7c363a74b85415704bc9b17e102876ea2812ba9a
mcontini <matteo.contini@zalando.de>
2020-10-10 16:04:59 +0200

    simplier infoPoint

--- src/client/components/tourExplanation/message/information/InfoPoint.jsx
+++ src/client/components/tourExplanation/message/information/InfoPoint.jsx
@@ -2,23 +2,12 @@ import React from 'react';
 import Slider from '../slider/Slider';
 import styles from './infoPoint.module.scss';
 
-const InfoPoint = ({ interest, getTarget }) => {
-    if (interest.pointOfInterestCoverImg) {
-        return (
-            <div className={styles.infoBox}>
-                <Slider images={interest.pointOfInterestGallery}/>
-                <div className={styles.title}>{interest.title}</div>
-                <div className={styles.text}>{interest.text}</div>
-            </div>
-        );
-    } else {
-        return (
-            <div onClick={getTarget} className={styles.infoBox}>
-                <div className={styles.title}> {interest.title}</div>
-                <div className={styles.text}>{interest.text}</div>
-            </div>
-        );
-    }
-};
+const InfoPoint = ({ interest }) => (
+    <div className={styles.infoBox}>
+        <Slider images={interest.pointOfInterestGallery}/>
+        <div className={styles.title}>{interest.title}</div>
+        <div className={styles.text}>{interest.text}</div>
+    </div>
+);
 
 export default InfoPoint;
