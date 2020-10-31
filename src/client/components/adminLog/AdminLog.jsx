03f2e471172c627cd20bc52a237be71ac696f0a6
mcontini <matteo.contini@zalando.de>
2020-10-31 12:28:12 +0100

    ignore config

--- src/client/components/adminLog/AdminLog.jsx
+++ src/client/components/adminLog/AdminLog.jsx
@@ -6,35 +6,39 @@ import {
     ButtonWrapper,
     InputButton,
 } from './AdminLog.styles';
+import firebaseConfig from '../../../firebase/config';
 
-const AdminLog = () => (
-    <Wrapper>
-        <h2>Inserisci i tuoi dati per creare un utente</h2>
-        <Form>
-            <Input
-                name="username"
-                type="text"
-                placeholder="Chose user name"
-                autoComplete="off"
-            />
-            <Input
-                name="email"
-                type="email"
-                placeholder="insert email"
-                autoComplete="off"
-            />
-            <Input
-                name="password"
-                type="password"
-                placeholder="Chose a password"
-                autoComplete="off"
-            />
-            <ButtonWrapper>
-                <InputButton>inserisci</InputButton>
-                <button>usa accoiunt esistente</button>
-            </ButtonWrapper>
-        </Form>
-    </Wrapper>
-);
+const AdminLog = () => {
+    console.log('firebaseConfig :>> ', firebaseConfig);
+    return (
+        <Wrapper>
+            <h2>Inserisci i tuoi dati per creare un utente</h2>
+            <Form>
+                <Input
+                    name="username"
+                    type="text"
+                    placeholder="Chose user name"
+                    autoComplete="off"
+                />
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="insert email"
+                    autoComplete="off"
+                />
+                <Input
+                    name="password"
+                    type="password"
+                    placeholder="Chose a password"
+                    autoComplete="off"
+                />
+                <ButtonWrapper>
+                    <InputButton>inserisci</InputButton>
+                    <button>usa accoiunt esistente</button>
+                </ButtonWrapper>
+            </Form>
+        </Wrapper>
+    );
+};
 
 export default AdminLog;
