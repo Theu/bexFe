ec4bde1250c9a8b45db56573f149b145e0ad7731
mcontini <matteo.contini@zalando.de>
2022-05-14 15:21:53 +0200

    reorganising mock to respect contentful schema

--- src/client/components/HomePage/Card/Card.tsx
+++ src/client/components/HomePage/Card/Card.tsx
@@ -15,18 +15,18 @@ interface Card {
   tourCover: {
     tourCardTitle: string,
     tourCardIntroText: string
+    tourCardImg: any
   }
 }
 
 const Card: React.FC<{card: Card}> = ({card}) => {
     const { tourName, tourCover } = card;
-    const { tourCardTitle, tourCardIntroText } = tourCover;
-    const cover = require(`../../../assets/${tourName}/cover.jpeg`);
+    const { tourCardTitle, tourCardIntroText, tourCardImg } = tourCover;
 
     return (
         <Link key={`${tourName}`} to={`${tourName}`}>
             <CardItem>
-                <Image src={cover} />
+                <Image src={tourCardImg} />
                 <CardText>
                     <CardTitle>{tourCardTitle}</CardTitle>
                     <CardIntroText>{tourCardIntroText}</CardIntroText>
