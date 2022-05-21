3561cd13dcb3f6ae8062ce5cd2a97b2b76ed2a90
mcontini <matteo.contini@zalando.de>
2022-05-21 11:56:31 +0200

    removed unused

--- src/client/components/ToursCreator/ToursCreator.styles.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import { maxDesktop } from '../../styles/variables';
-
-export const Wrapper = styled.div`
-    width: ${maxDesktop}px;
-    margin: 10px auto;
-    display: flex;
-    flex-direction: column;
-`;
